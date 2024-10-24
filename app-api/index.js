4757b528c76529e58e44fd71e04754a1fd698d96
gaurav <gk17265@gmail.com>
2024-10-24 18:30:45 +0530

    added swagger

--- app-api/index.js
+++ app-api/index.js
@@ -2,10 +2,15 @@ import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
 import router from "./app/routes/api.js";
+import swaggerUi from "swagger-ui-express";
+import { readFileSync } from "fs";
 import sequelize from "./app/models/index.js";
 import logger from "./app/lib/logger.js";
 
 config();
+
+const swaggerDocument = readFileSync("./swagger/swagger-output.json", "utf8");
+const swaggerJson = JSON.parse(swaggerDocument);
 
 const app = express();
 
@@ -14,6 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 app.all("*", (req, res) => {
   return res.status(404).send({
